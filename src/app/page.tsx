ca4d109cbd42d474ac46a3b62e6b081bf67c9973
Nigel Petargue <nigelpetargue@gmail.com>
2024-08-23 23:22:54 +0800

    initialize ui framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
@@ -107,6 +108,8 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+
+        <ModeToggle />
       </div>
     </main>
   );
